0fa4f5db655702119600b1d8990b39cf5cbb5d4d
Yokin <minucia.clerigos.0b@icloud.com>
2023-01-16 16:48:59 +0100

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Home from "./pages/Home";
+import Select from "./pages/Select";
+import Game from "./pages/Game";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
-  return;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/select" element={<Select />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
