03d42eeae9fe19a6965249aba75f785866b88e46
Yokin <minucia.clerigos.0b@icloud.com>
2023-01-17 11:57:11 +0100

    feat: add functions to Home page

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,1 +1,5 @@
-export default function Game() {}
+export default function Game() {
+  const name = localStorage.getItem("Name");
+
+  return <div>Mi mascota se llama {name}</div>;
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
-  return <div>Desde home</div>;
+  const toGame = useNavigate();
+
+  const setName = (e) => {
+    const name = e.target.value;
+    localStorage.setItem("Name", name);
+  };
+
+  const play = () => {
+    toGame("/game");
+  };
+
+  return (
+    <div>
+      <h1>Train your dragon</h1>
+      <input
+        type="text"
+        placeholder="Enter a name"
+        onChange={(e) => setName(e)}
+      />
+      <button onClick={play}>PLAY</button>
+    </div>
+  );
 }
