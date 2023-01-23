3f76559f7251e419568d4ff2ba0e4bb28f284d2a
Yokin <minucia.clerigos.0b@icloud.com>
2023-01-23 18:32:19 +0100

    feat: update functions on Game page

--- src/components/StatusBars/StatusBars.jsx
+++ src/components/StatusBars/StatusBars.jsx
@@ -7,33 +7,9 @@ const name = localStorage.getItem("Name");
 
 export default function StatusBars({
   health,
-  setHealth,
   happiness,
-  setHappiness,
   sleep,
-  setSleep,
 }) {
-  if (health >= 1) {
-    setTimeout(() => {
-      setHealth(health - 1);
-      localStorage.setItem("Health", health);
-    }, 1000);
-  }
-
-  if (happiness >= 1) {
-    setTimeout(() => {
-      setHappiness(happiness - 1);
-      localStorage.setItem("Happiness", happiness);
-    }, 1000);
-  }
-
-  if (sleep >= 1) {
-    setTimeout(() => {
-      setSleep(sleep - 1);
-      localStorage.setItem("Sleep", sleep);
-    }, 1000);
-  }
-
   return (
     <S.Container>
       <S.Name>{name}</S.Name>
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -9,15 +9,34 @@ export default function Game() {
   const [happiness, setHappiness] = useState(50);
   const [sleep, setSleep] = useState(100);
 
+  if (health >= 1) {
+    setTimeout(() => {
+      setHealth(health - 1);
+    }, 2000);
+  }
+
+  if (happiness >= 1) {
+    setTimeout(() => {
+      setHappiness(happiness - 1);
+    }, 3000);
+  }
+
+  if (sleep >= 1) {
+    setTimeout(() => {
+      setSleep(sleep - 1);
+    }, 4000);
+  }
+
+  localStorage.setItem("Health", health);
+  localStorage.setItem("Happiness", happiness);
+  localStorage.setItem("Sleep", sleep);
+
   return (
     <S.Container>
       <StatusBars
         health={health}
         happiness={happiness}
         sleep={sleep}
-        setHealth={setHealth}
-        setHappiness={setHappiness}
-        setSleep={setSleep}
       />
       <Moods />
       <Buttons
