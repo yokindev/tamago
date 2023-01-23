3ec907d949a405d62b95d77ef49182c66c406ce3
Yokin <minucia.clerigos.0b@icloud.com>
2023-01-23 20:09:54 +0100

    feat: pass props to Moods component

--- src/components/Moods/Moods.jsx
+++ src/components/Moods/Moods.jsx
@@ -1,6 +1,6 @@
 import Dragon from "../../assets/images/hello.png";
 import * as S from "./Moods.styles";
 
-export default function Moods() {
+export default function Moods({health, happiness, sleep}) {
   return <S.Image src={Dragon} />;
 }
