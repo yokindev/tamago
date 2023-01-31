231ac717256076d2e79129b1f8b3f21b5f0c0632
Yokin <minucia.clerigos.0b@icloud.com>
2023-01-31 10:20:08 +0100

    feat: add media query breakpoints

--- src/styles/Home.styles.js
+++ src/styles/Home.styles.js
@@ -5,41 +5,64 @@ export const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-evenly;
   background-color: #e0fbfc;
+
+  @media (max-width: 768px) {
+    justify-content: space-around;
+  }
 `;
 
 export const HomeTitle = styled.h1`
   margin: 0;
   text-align: center;
-  font-size: 45px;
+  font-size: 75px;
   font-weight: 900;
   background: -webkit-linear-gradient(270deg, #3d5a80, #98c1d9, #ee6c4d);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
+
+  @media (max-width: 768px) {
+    font-size: 45px;
+  }
 `;
 
 export const HomeImage = styled.img`
-  width: 300px;
+  width: 500px;
+
+  @media (max-width: 768px) {
+    width: 300px;
+  }
 `;
 
 export const HomeInput = styled.input`
   border: 3px solid #3d5a80;
-  width: 200px;
+  width: 400px;
   border-radius: 50px;
-  padding: 15px;
+  padding: 25px;
   font-weight: 900;
-  font-size: 15px;
+  font-size: 25px;
   text-align: center;
+
+  @media (max-width: 768px) {
+    width: 200px;
+    padding: 15px;
+    font-size: 15px;
+  }
 `;
 
 export const HomeButton = styled.button`
   border: none;
-  padding: 15px;
+  padding: 25px;
   border-radius: 50px;
   background-color: #ee6c4d;
   font-weight: 900;
   color: #e0fbfc;
-  font-size: 25px;
+  font-size: 35px;
   cursor: pointer;
+
+  @media (max-width: 768px) {
+    padding: 15px;
+    font-size: 25px;
+  }
 `;
