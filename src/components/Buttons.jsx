fea95d5326fb649d317acccfb0a46b6932d0947d
Yokin <minucia.clerigos.0b@icloud.com>
2023-02-01 18:46:02 +0100

    feat: pass functions from Buttons to Game

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,121 +2,28 @@ import IconFood from "../assets/icons/food.png";
 import IconMusic from "../assets/icons/music.png";
 import IconBed from "../assets/icons/bed.png";
 import IconMedicine from "../assets/icons/medicine.png";
-import DragonDefault from "../assets/images/default.png";
-import DragonEat from "../assets/images/eat.png";
-import DragonSing from "../assets/images/sing.png";
-import DragonRest from "../assets/images/rest.png";
-import DragonYoga from "../assets/images/yoga.png";
-import DragonNo from "../assets/images/no.png";
+
 import {
   ButtonsContainer,
   ButtonsBox,
   ButtonsIcon,
 } from "../styles/Buttons.styles";
 
-export default function Buttons({
-  health,
-  setHealth,
-  happiness,
-  setHappiness,
-  sleep,
-  setSleep,
-  setImage,
-}) {
-  const eat = () => {
-    if (health <= 75) {
-      setHealth((health) => health + 25);
-      setImage(DragonEat);
-
-      setTimeout(() => {
-        setImage(DragonDefault);
-      }, 2000);
-    } else {
-      setImage(DragonNo);
-
-      setTimeout(() => {
-        setImage(DragonDefault);
-      }, 2000);
-    }
-  };
-
-  const sing = () => {
-    if (happiness <= 75) {
-      setHappiness((happiness) => happiness + 25);
-      setImage(DragonSing);
-
-      setTimeout(() => {
-        setImage(DragonDefault);
-      }, 2000);
-    } else {
-      setImage(DragonNo);
-
-      setTimeout(() => {
-        setImage(DragonDefault);
-      }, 2000);
-    }
-  };
-
-  const rest = () => {
-    if (sleep <= 50) {
-      setSleep((sleep) => sleep + 50);
-      setImage(DragonRest);
-
-      setTimeout(() => {
-        setImage(DragonDefault);
-      }, 2000);
-    } else {
-      setImage(DragonNo);
-
-      setTimeout(() => {
-        setImage(DragonDefault);
-      }, 2000);
-    }
-  };
-
-  const medicine = () => {
-    if (health <= 50) {
-      setHealth((health) => health + 50);
-      setImage(DragonYoga);
-
-      setTimeout(() => {
-        setImage(DragonDefault);
-      }, 2000);
-    } else {
-      setImage(DragonNo);
-
-      setTimeout(() => {
-        setImage(DragonDefault);
-      }, 2000);
-    }
-  };
-
-  const buttons = [
-    {
-      function: eat,
-      icon: IconFood,
-    },
-    {
-      function: sing,
-      icon: IconMusic,
-    },
-    {
-      function: rest,
-      icon: IconBed,
-    },
-    {
-      function: medicine,
-      icon: IconMedicine,
-    },
-  ];
-
+export default function Buttons({ eat, sing, rest, medicine }) {
   return (
     <ButtonsContainer>
-      {buttons.map((button, index) => (
-        <ButtonsBox key={index} onClick={button.function}>
-          <ButtonsIcon src={button.icon} />
-        </ButtonsBox>
-      ))}
+      <ButtonsBox onClick={eat}>
+        <ButtonsIcon src={IconFood} />
+      </ButtonsBox>
+      <ButtonsBox onClick={sing}>
+        <ButtonsIcon src={IconMusic} />
+      </ButtonsBox>
+      <ButtonsBox onClick={rest}>
+        <ButtonsIcon src={IconBed} />
+      </ButtonsBox>
+      <ButtonsBox onClick={medicine}>
+        <ButtonsIcon src={IconMedicine} />
+      </ButtonsBox>
     </ButtonsContainer>
   );
 }
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,6 +2,11 @@ import StatusBars from "../components/StatusBars";
 import Moods from "../components/Moods";
 import Buttons from "../components/Buttons";
 import DragonDefault from "../assets/images/default.png";
+import DragonEat from "../assets/images/eat.png";
+import DragonSing from "../assets/images/sing.png";
+import DragonRest from "../assets/images/rest.png";
+import DragonYoga from "../assets/images/yoga.png";
+import DragonNo from "../assets/images/no.png";
 import DragonDead from "../assets/images/dead.png";
 import {
   GameContainer,
@@ -23,9 +28,78 @@ export default function Game() {
   localStorage.setItem("Sleep", sleep);
   localStorage.setItem("Image", image);
 
-  const name = (localStorage.getItem("Name")).toUpperCase();
+  const name = localStorage.getItem("Name").toUpperCase();
+
+  const eat = () => {
+    if (health <= 75) {
+      setHealth((health) => health + 25);
+      setImage(DragonEat);
+
+      setTimeout(() => {
+        setImage(DragonDefault);
+      }, 2000);
+    } else {
+      setImage(DragonNo);
+
+      setTimeout(() => {
+        setImage(DragonDefault);
+      }, 2000);
+    }
+  };
+
+  const sing = () => {
+    if (happiness <= 75) {
+      setHappiness((happiness) => happiness + 25);
+      setImage(DragonSing);
+
+      setTimeout(() => {
+        setImage(DragonDefault);
+      }, 2000);
+    } else {
+      setImage(DragonNo);
+
+      setTimeout(() => {
+        setImage(DragonDefault);
+      }, 2000);
+    }
+  };
+
+  const rest = () => {
+    if (sleep <= 50) {
+      setSleep((sleep) => sleep + 50);
+      setImage(DragonRest);
+
+      setTimeout(() => {
+        setImage(DragonDefault);
+      }, 2000);
+    } else {
+      setImage(DragonNo);
+
+      setTimeout(() => {
+        setImage(DragonDefault);
+      }, 2000);
+    }
+  };
+
+  const medicine = () => {
+    if (health <= 50) {
+      setHealth((health) => health + 50);
+      setImage(DragonYoga);
+
+      setTimeout(() => {
+        setImage(DragonDefault);
+      }, 2000);
+    } else {
+      setImage(DragonNo);
+
+      setTimeout(() => {
+        setImage(DragonDefault);
+      }, 2000);
+    }
+  };
 
   const navigate = useNavigate();
+  
   const back = () => {
     navigate("/");
     localStorage.clear();
@@ -50,15 +124,7 @@ export default function Game() {
             image={image}
             setImage={setImage}
           />
-          <Buttons
-            health={health}
-            happiness={happiness}
-            sleep={sleep}
-            setHealth={setHealth}
-            setHappiness={setHappiness}
-            setSleep={setSleep}
-            setImage={setImage}
-          />
+          <Buttons eat={eat} sing={sing} rest={rest} medicine={medicine}/>
         </GameContainer>
       ) : (
         <GameContainer>
